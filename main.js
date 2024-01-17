03fa1c9f31aec63785c712190385740a89de6862
Devilal panchal <devilalpanchal551@gmail.com>
2024-01-17 19:03:49 +0530

    add files

---
+++ main.js
@@ -0,0 +1,21 @@
+let answer = document.querySelector(".answer")
+console.log(answer)
+console.log("helllo")
+let btn = document.querySelectorAll(".btn")
+console.log(btn)
+
+
+btn.forEach((item)=>{
+
+    item.addEventListener("click",(e)=>{
+        console.log("click")
+        let ans=e.currentTarget.parentElement.parentElement.nextSibling.nextSibling
+        console.log(ans)
+        ans.classList.toggle('hide')
+let currentbtn =document.querySelector(".plus")
+let otherbtn =document.querySelector(".minus")
+console.log(otherbtn)
+currentbtn.classList.toggle('hide')
+otherbtn.classList.toggle('hide')
+    })
+})
